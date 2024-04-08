a5e5eaa408aad192d12942e1fced7874f509277c
Abhinau Kumar <ab.kumr98@gmail.com>
2024-04-07 20:39:30 -0500

    Script to download checkpoints and images

---
+++ download_data.py
@@ -0,0 +1,8 @@
+import gdown
+
+def main():
+    gdown.download_folder(id='18hDTnbsydsegVJ8-_mrniKfcO_T90ZH0', output='DisQUE_Checkpoints')
+    gdown.download_folder(id='1oAtKSy57CTcM1ssR3m7RrZYjs-VOX4t_', output='DisQUE_Images')
+
+if __name__ == '__main__':
+    main()
